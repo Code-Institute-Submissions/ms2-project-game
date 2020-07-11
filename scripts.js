68a98d3ed01cfa144d9e63eb80c13ed76de84202
Dorcas-Amoo <doorcars@gmail.com>
2020-07-11 03:12:41 +0000

    Initial code to allow html page to load first

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/*The codes below were largely influenced by tutorials from personal research and adapted to achieve my desired outcome, please refer to references in the README.md, thank you.:)*/
+
+//Allows HTML page to load first
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
